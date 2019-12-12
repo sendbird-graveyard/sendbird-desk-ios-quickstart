7e64c8ebd40553dbc31fcd4ab6bad7710ebdc875
Rommel Sunga <rommel.sunga@sendbird.com>
2019-12-12 09:29:35 +0900

    Initial Commit

---
+++ SendBirdDeskTestBed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SendBirdDeskTestBed
+//
+//  Created by Rommel Sunga on 12/12/19.
+//  Copyright © 2019 Rommel Sunga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
